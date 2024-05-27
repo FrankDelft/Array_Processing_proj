6c660801a1e864f48552b17713c07d2b6ab4448b
Frank <f.r.harraway@student.tudelft.nl>
2024-05-27 16:34:07 +0200

    neatened it up

--- proj1/signal_model.m
+++ proj1/signal_model.m
@@ -1,82 +1,45 @@
 % Test parameters
 M = 5; % Number of antennas
 N = 20; % Number of samples
-theta = [-50 * (pi/180); 50 * (pi/180)]; % Directions of arrival in radians
+theta = [-20 * (pi/180); 30 * (pi/180)]; % Directions of arrival in radians
 f = [0.1; 0.3]; % Normalized frequencies
 SNR = [20; 20]; % Signal-to-noise ratios in dB
-[X, A, S] = gen_data(M, N, 0.5, theta, f, SNR);
 
-% Perform SVD and plot singular values
-[U, Singular, V] = svd(X);
-[U_s, Singular_s, V_s] = svd(S);
+% Generate data and perform SVD for original data
+[X, ~, ~] = gen_data(M, N, 0.5, theta, f, SNR);
+[~, Singular] = svd(X);
 
+% Double samples
+N = 40;
+[X_sample, ~, ~] = gen_data(M, N, 0.5, theta, f, SNR);
+[~, Singular_sample] = svd(X_sample);
 
-%double samples
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-N = 40; % Number of samples
-[X_sample, A_sample, S_sample] = gen_data(M, N, 0.5, theta, f, SNR);
+% Double antennas
+M = 10; N = 20;
+[X_ant, ~, ~] = gen_data(M, N, 0.5, theta, f, SNR);
+[~, Singular_ant] = svd(X_ant);
 
-% Perform SVD and plot singular values
-[ ~, Singular_sample,~ ] = svd(X_sample);
+% Make source angles closer
+M = 5; theta = [-5 * (pi/180); 10 * (pi/180)];
+[X_angle, ~, ~] = gen_data(M, N, 0.5, theta, f, SNR);
+[~, Singular_angle] = svd(X_angle);
 
+% Make frequencies closer
+theta = [-20 * (pi/180); 30 * (pi/180)]; f = [0.27; 0.3];
+[X_freq, ~, ~] = gen_data(M, N, 0.5, theta, f, SNR);
+[~, Singular_freq] = svd(X_freq);
 
-%double antennas
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-M = 10; % Number of antennas
-N = 20; % Number of samples
-[X_freq, A_freq, S_freq] = gen_data(M, N, 0.5, theta, f, SNR);
+% Store singular values and titles in arrays
+singular_values = {diag(Singular), diag(Singular_sample), diag(Singular_ant), diag(Singular_angle), diag(Singular_freq)};
+titles = ["Original Data", "Doubled Samples", "Doubled Antennas", "Closer Source Angles", "Closer Frequencies"];
 
-% Perform SVD and plot singular values
-[ ~, Singular_freq,~ ] = svd(X_freq);
-
-%make source angles closer
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-M = 5; % Number of antennas
-theta = [-5 * (pi/180); 10 * (pi/180)]; % Directions of arrival in radians
-[X_angle, A_angle, S_angle] = gen_data(M, N, 0.5, theta, f, SNR);
-
-% Perform SVD and plot singular values
-[ ~, Singular_angle,~ ] = svd(X_angle);
-
-
-
-% Define the subplot arrangement
-subplot_rows = 2; % Two rows for the different cases
-subplot_cols = 2; % Two columns for the different cases
-
-% Plotting the singular values
+% Create a figure and plot all the singular values in subplots
 figure;
-
-% Plot for original data
-subplot(subplot_rows, subplot_cols, 1);
-stem(diag(Singular));
-title('Original Data');
-xlabel('Index');
-ylabel('Singular Value');
-
-% Plot for doubled samples
-subplot(subplot_rows, subplot_cols, 2);
-stem(diag(Singular_sample));
-title('Doubled Samples');
-xlabel('Index');
-ylabel('Singular Value');
-
-% Plot for doubled antennas
-subplot(subplot_rows, subplot_cols, 3);
-stem(diag(Singular_freq));
-title('Doubled Antennas');
-xlabel('Index');
-ylabel('Singular Value');
-
-% Plot for closer source angles
-subplot(subplot_rows, subplot_cols, 4);
-stem(diag(Singular_angle));
-title('Closer Source Angles');
-xlabel('Index');
-ylabel('Singular Value');
-
-
-
-
-
-
+num_plots = length(singular_values);
+for i = 1:num_plots
+    subplot(ceil(num_plots/2), 2, i); % Arrange subplots in a grid
+    stem(singular_values{i});
+    title(titles(i));
+    xlabel('Index');
+    ylabel('Singular Value');
+end
