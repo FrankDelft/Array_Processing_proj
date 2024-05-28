3b274e10fc97b751a90171cc40057d95d7c28422
Frank <f.r.harraway@student.tudelft.nl>
2024-05-28 11:00:03 +0200

    got the esprit DOA to work

--- proj1/esprit.m
+++ proj1/esprit.m
@@ -6,15 +6,19 @@ function theta = esprit(X,d)
     Y=X(2:M,:);
 
     Z=[X_1;Y];
-    %[U_z,Sigma_z,V_z]=svd(Z);
-    %U_z_hat=U_z(:,1:d);
+    [U_z,Sigma_z,V_z]=svd(Z);
+    U_z_hat=U_z(:,1:d);
+    U_x_hat=U_z_hat(1:4,:);
+    U_y_hat=U_z_hat(5:end,:);
 
-    [U_x,Sigma_x,V_x]=svd(X_1);
-    [U_y,Sigma_y,V_y]=svd(Y);
-    U_x_hat=U_x(:,1:d);
-    U_y_hat=U_y(:,1:d);
+
+    %[U_x,Sigma_x,V_x]=svd(X_1);
+    %[U_y,Sigma_y,V_y]=svd(Y);
+    %U_x_hat=U_x(:,1:d);
+    %U_y_hat=U_y(:,1:d);
     
-    temp=(ctranspose(U_x_hat)*U_x_hat)*ctranspose(U_x_hat)*U_y_hat;
+    %temp=(ctranspose(U_x_hat)*U_x_hat)*ctranspose(U_x_hat)*U_y_hat;
+    temp=pinv(U_x_hat)*U_y_hat;
     [T,theta]=eig(temp);
     theta=diag(theta)
 end
